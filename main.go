dd3cab1fa18fe5e07fc47fe9a380b081cb9b687d
Saiqi <jsq2627@gmail.com>
2014-07-24 13:46:42 +0800

    Better user prompt.

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -30,96 +30,111 @@ func getViewState(s *string) string {
 	return match[0][1]
 }
 
-func (self *StudentInfo) ScanFromInput() error {
-	fmt.Print("请输入你的学号: ")
-	fmt.Scan(&self.Number)
-	for _, v := range self.Number {
-		if !unicode.IsDigit(v) {
-			return errors.New("学号无效，请重新输入。")
+func (self *StudentInfo) ScanFromInput() {
+OUTER:
+	for {
+		fmt.Print("请输入你的学号: ")
+		fmt.Scan(&self.Number)
+		for _, v := range self.Number {
+			if !unicode.IsDigit(v) {
+				fmt.Println("学号无效，请重新输入。")
+				continue OUTER
+			}
 		}
+
+		fmt.Print("请输入你的教务系统登录密码: ")
+		fmt.Scan(&self.Password)
+		break
 	}
 
-	fmt.Print("请输入你的教务系统登录密码: ")
-	fmt.Scan(&self.Password)
-
-	return nil
 }
 
 func main() {
 	var student StudentInfo
 
 	// Input number and password
-	for {
-		err := student.ScanFromInput()
-		if err == nil {
-			break
-		}
-		fmt.Println(err)
-	}
+	student.ScanFromInput()
 
 	// Input the subject name to check
 	var subjectName string
 	fmt.Print("请输入要查询的课程名称: ")
 	fmt.Scan(&subjectName)
 
+	log.Println("开始尝试...")
+
+	var lastUrl *url.URL
+	var lastBodyContent string
+	var client *http.Client
+
 	// Handle panic
 	defer func() {
 		if err := recover(); err != nil {
-			log.Fatal("发生异常，错误如下:\n", err)
+			log.Println("发生异常，错误如下:\n", err)
+			fmt.Println("输出调试信息？(y/n)")
+			var choice string
+			if fmt.Scan(&choice); choice == "y" {
+				fmt.Println(lastBodyContent)
+			}
+			os.Exit(1)
 		}
 	}()
 
-	var lastUrl *url.URL
-	var client *http.Client
-
-	redirectCheck := func(req *http.Request, via []*http.Request) error {
-		lastUrl = req.URL
-		return nil
-	}
-
 	client = &http.Client{
-		CheckRedirect: redirectCheck,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			lastUrl = req.URL
+			return nil
+		},
 	}
 
+	// Get login entrance
 	resp, err := client.Get(baseUrl)
 	defer resp.Body.Close()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	loginUrl := lastUrl
-	client.CheckRedirect = nil
-	fmt.Println("取得教务登录 URL:", loginUrl)
+	log.Println("取得教务登录入口:", loginUrl)
 
+	// Get login hash
 	reg := regexp.MustCompile("\\/%28(.*)%29\\/")
 	match := reg.FindAllStringSubmatch(loginUrl.String(), 1)
 	loginHash := match[0][1]
-	fmt.Println("登录状态 Hash:", loginHash)
+	log.Println("Hash:", loginHash)
 
 	// Get __VIEWSTATE
 	body, _ := ioutil.ReadAll(resp.Body)
-	bodyStr := string(body)
-	viewState := getViewState(&bodyStr)
-	loginFormValues := url.Values{"__VIEWSTATE": {viewState}, "TextBox1": {student.Number}, "TextBox2": {student.Password}, "TextBox3": {""}, "Button1": {""}, "lbLanguage": {""}, "RadioButtonList1": {"学生"}}
+	lastBodyContent = string(body)
+	viewState := getViewState(&lastBodyContent)
 
-	// Do login
-	resp, err = client.PostForm(loginUrl.String(), loginFormValues)
-	if err != nil {
-		log.Fatal(err)
+	for {
+		// Do login
+		loginFormValues := url.Values{"__VIEWSTATE": {viewState}, "TextBox1": {student.Number}, "TextBox2": {student.Password}, "TextBox3": {""}, "Button1": {""}, "lbLanguage": {""}, "RadioButtonList1": {"学生"}}
+		resp, err = client.PostForm(loginUrl.String(), loginFormValues)
+		defer resp.Body.Close()
+		if err != nil {
+			panic(err)
+		}
+
+		// Get student number and name
+		cd, _ := iconv.Open("utf-8", "gbk")
+		body, _ = ioutil.ReadAll(resp.Body)
+		lastBodyContent = cd.ConvString(string(body))
+		reg = regexp.MustCompile("\\<span id=\"xhxm\"\\>(\\d*)  (.*)同学\\<\\/span\\>")
+		match = reg.FindAllStringSubmatch(lastBodyContent, 1)
+		if len(match) == 0 {
+			log.Println("登录失败，请重新输入学号密码。")
+			student.ScanFromInput()
+			continue
+		}
+		log.Println("已登录，姓名:", match[0][2])
+		break
 	}
-
-	// Get student number and name
-	cd, _ := iconv.Open("utf-8", "gbk")
-	body, _ = ioutil.ReadAll(resp.Body)
-	bodyStr = cd.ConvString(string(body))
-	reg = regexp.MustCompile("\\<span id=\"xhxm\"\\>(\\d*)  (.*)同学\\<\\/span\\>")
-	match = reg.FindAllStringSubmatch(bodyStr, 1)
-	fmt.Println("已登录，姓名:", match[0][2])
 
 	// Get grade query URL
 	reg = regexp.MustCompile("href=\"(xscjcx.aspx\\?xh=\\d*&xm=.*&gnmkdm=.*)\" target.*成绩查询")
-	match = reg.FindAllStringSubmatch(bodyStr, 1)
+	match = reg.FindAllStringSubmatch(lastBodyContent, 1)
 	gradeQueryUrlStr := match[0][1]
-	fmt.Println("成绩查询入口:", gradeQueryUrlStr)
+	log.Println("取得成绩查询入口:", gradeQueryUrlStr)
 
 	// Load grade page to get __VIEWSTATE
 	gradeQueryUrl, _ := url.Parse(baseUrl + "(" + loginHash + ")/" + gradeQueryUrlStr)
@@ -127,14 +142,15 @@ func main() {
 	req, _ := http.NewRequest("GET", gradeQueryUrl.String(), bytes.NewBufferString(""))
 	req.Header.Add("Referer", refererUrl.String())
 	resp, err = client.Do(req)
+	defer resp.Body.Close()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	// Get __VIEWSTATE
 	body, _ = ioutil.ReadAll(resp.Body)
-	bodyStr = string(body)
-	viewState = getViewState(&bodyStr)
+	lastBodyContent = string(body)
+	viewState = getViewState(&lastBodyContent)
 
 	queryFormValues := url.Values{
 		"__EVENTTARGET":   {""},
@@ -154,17 +170,17 @@ func main() {
 	req.Header.Add("Referer", gradeQueryUrl.String())
 	resp, err = client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	cd, _ = iconv.Open("utf-8", "gbk")
+	cd, _ := iconv.Open("utf-8", "gbk")
 	body, _ = ioutil.ReadAll(resp.Body)
-	gradeResult := cd.ConvString(string(body))
+	lastBodyContent = cd.ConvString(string(body))
 
-	count := strings.Count(gradeResult, subjectName)
+	count := strings.Count(lastBodyContent, subjectName)
 	if count > 0 {
-		fmt.Println(subjectName + "成绩已出！")
+		log.Println(subjectName + "成绩已出！")
 	} else {
-		fmt.Println(subjectName + "成绩还没出。")
+		log.Println(subjectName + "成绩还没出。")
 	}
 }
